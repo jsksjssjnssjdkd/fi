036bebf8ec0b7da760e69222601a4e85dbb5574c
jsksjssjnssjdkd <101276719+jsksjssjnssjdkd@users.noreply.github.com>
2022-07-17 07:08:53 +0200

    Update Scolze.py

--- YukkiMusic/plugins/play/Scolze.py
+++ YukkiMusic/plugins/play/Scolze.py
@@ -1,3 +1,4 @@
+
 import asyncio
 from pyrogram import Client, filters
 from strings import get_command
@@ -7,22 +8,57 @@ from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, ap
 
 
 @app.on_message(
-     command(["مبرمج السورس","مطور السورس","المطور","المبرمج"])
+     command(["مبرمج السورس","المطور","مطور السورس","المبرمج"])
     & filters.group
     & ~filters.edited
 )
 async def khalid(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/a6c96cdbd066ca2388d06.jpg",
-         caption=f"""◍ مش محتاجين نكتب كلام كتير خش ع اول زرار وانت هتعرف""",
+        caption=f"""◍ الزرار الاول: قناه السورس \n◍ الزرار الثاني: هو مبرمج السورس\n√""",
         reply_markup=InlineKeyboardMarkup(
-         [
             [
-                InlineKeyboardButton("• 𝘿𝙀𝙑𝙀𝙇𝙊𝙋𝙀𝙍 ☤ ", url=f"https://t.me/WORLD_MUSIC_F"),
-            ],
-            [
-                InlineKeyboardButton("ضيـف البـوت لمجمـوعتـك ✅", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
+                [
+                InlineKeyboardButton(
+                    "🔮𝐒𝐎𝐔𝐑𝐂𝐄🔮", url=f"https://t.me/ch_world_music"
+                ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "♡اضف البوت الى مجموعتك♡", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
+                ],
             ]
-         ]
-     )
-  )
+        ),
+    )
+ort asyncio
+from pyrogram import Client, filters
+from strings import get_command
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
+
+@app.on_message(
+     command(["مبرمج السورس","المطور","مطور السورس","المبرمج"])
+         & filters.group
+             & ~filters.edited
+             )
+             async def khalid(client: Client, message: Message):
+                 await message.reply_photo(
+                         photo=f"https://telegra.ph/file/a6c96cdbd066ca2388d06.jpg",
+                                 caption=f"""◍ الزرار الاول: قناه السورس \n◍ الزرار الثاني: هو مبرمج السورس\n√""",
+                                         reply_markup=InlineKeyboardMarkup(
+                                                     [
+                                                                     [
+                                                                                     InlineKeyboardButton(
+                                                                                                         "🔮𝐒𝐎𝐔𝐑𝐂𝐄🔮", url=f"https://t.me/ch_world_music"
+                                                                                                                         ),
+                                                                                                                                         ],
+                                                                                                                                                         [
+                                                                                                                                                                             InlineKeyboardButton(
+                                                                                                                                                                                                     "♡اضف البوت الى مجموعتك♡", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
+                                                                                                                                                                                                                     ],
+                                                                                                                                                                                                                                 ]
+                                                                                                                                                                                                                                         ),
+                                                                                                                                                                                                                                             )
+                                                                                                                                                                                                                                             
