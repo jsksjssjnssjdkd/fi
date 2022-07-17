170489342482aa3bb4c5fc322ab800b7d1bd6b45
jsksjssjnssjdkd <101276719+jsksjssjnssjdkd@users.noreply.github.com>
2022-07-17 07:18:58 +0200

    Update Scolze.py

--- YukkiMusic/plugins/play/Scolze.py
+++ YukkiMusic/plugins/play/Scolze.py
@@ -30,35 +30,3 @@ async def khalid(client: Client, message: Message):
             ]
         ),
     )
-ort asyncio
-from pyrogram import Client, filters
-from strings import get_command
-from strings.filters import command
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-
-
-@app.on_message(
-     command(["مبرمج السورس","المطور","مطور السورس","المبرمج"])
-         & filters.group
-             & ~filters.edited
-             )
-             async def khalid(client: Client, message: Message):
-                 await message.reply_photo(
-                         photo=f"https://telegra.ph/file/a6c96cdbd066ca2388d06.jpg",
-                                 caption=f"""◍ الزرار الاول: قناه السورس \n◍ الزرار الثاني: هو مبرمج السورس\n√""",
-                                         reply_markup=InlineKeyboardMarkup(
-                                                     [
-                                                                     [
-                                                                                     InlineKeyboardButton(
-                                                                                                         "🔮𝐒𝐎𝐔𝐑𝐂𝐄🔮", url=f"https://t.me/ch_world_music"
-                                                                                                                         ),
-                                                                                                                                         ],
-                                                                                                                                                         [
-                                                                                                                                                                             InlineKeyboardButton(
-                                                                                                                                                                                                     "♡اضف البوت الى مجموعتك♡", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
-                                                                                                                                                                                                                     ],
-                                                                                                                                                                                                                                 ]
-                                                                                                                                                                                                                                         ),
-                                                                                                                                                                                                                                             )
-                                                                                                                                                                                                                                             
